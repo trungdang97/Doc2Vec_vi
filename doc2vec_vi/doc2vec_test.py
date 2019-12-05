e9573974759f8259ab51201423cd4ce009b1ad96
trungdang97@gmail.com <trungdang97@gmail.com>
2019-12-05 22:49:26 +0700

    update comment doc2vec_vi

--- doc2vec_vi/doc2vec_test.py
+++ doc2vec_vi/doc2vec_test.py
@@ -6,25 +6,21 @@ import random
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument, Doc2VecKeyedVectors
 from scipy import spatial
 
-model_path = "doc2vec_vi/models/corpus-title.model"
-corpus_path = "datasets/vi/corpus-title-small.tok.txt"
+model_path = "doc2vec_vi/models/corpus-title.model" #(SHOULD BE MODIFIED)
+corpus_path = "datasets/vi/corpus-title-small.tok.txt" #(SHOULD BE MODIFIED)
 
 # load corpus into program
-
-
 def LoadCorpus(path):
     f = open(path, "r", encoding='utf-8')
     return f.readlines()
 
-# get n random sentences from corpus
-
-
+# get n random sentences from corpus (SHOULD BE MODIFIED)
 def PrepareBatch():
     sentences = corpus.copy()
     for i, s in enumerate(sentences):
-        # TaggedDocument(tokens,tags) both params are lists
         # Split by spaces then remove the LMU concatenation symbol '_'
         sentences[i] = sentences[i].split()
+        # TaggedDocument(tokens,tags) both params are lists
         sentences[i] = [w.replace('_', ' ').lower() for w in sentences[i]]
     return sentences
 
@@ -32,12 +28,13 @@ corpus = LoadCorpus(corpus_path)
 batch = PrepareBatch()
 model = Doc2Vec.load(model_path)
 
-sentence = ['nghiên cứu','tiện ích']
+sentence = ['nghiên cứu','tiện ích'] # sentence need to be vectorized, splitted in to tokens
 
-vector = model.infer_vector([w.lower() for w in sentence], steps=100, alpha=0.05)
+vector = model.infer_vector([w.lower() for w in sentence], steps=100, alpha=0.05) # vector of the sentence
 
 evaluation = []
 
+# a type that consist sentence and its similarity to vector above
 class eval:
     sentence = ''
     similarity = 0
@@ -47,10 +44,11 @@ class eval:
 
 for i, s in enumerate(batch):
     v = model.infer_vector([w.lower() for w in s], steps=100, alpha=0.05)
-    cs = 1 - spatial.distance.cosine(vector,v)
+    cs = 1 - spatial.distance.cosine(vector,v) # calculate the cosine similarity: similarity = 1 - distance
     evaluation.append(eval(corpus[i], cs))
     #print(corpus[i],cs)
 
+# print the evaluations of similarity
 evaluation = sorted(evaluation, key=lambda x: x.similarity, reverse=True)
 for item in evaluation:
     print('{0:.2f} {1}'.format(item.similarity,item.sentence))
--- doc2vec_vi/doc2vec_train.py
+++ doc2vec_vi/doc2vec_train.py
@@ -9,42 +9,37 @@ from sklearn import metrics
 
 corpus = []
 
-batch_number = 1000
+batch_number = 1000 # number of sentences take from corpus (SHOULD BE MODIFIED)
 
+# HYPERPARAMS
 model = Doc2Vec()
-vector_size = 50
-min_count = 2
-epochs = 100
-dm = 1 # use PV-DM
-alpha = 0.025
+vector_size = 50 # word vector dimension
+min_count = 2 # word count
+epochs = 100 # update times for a batch
+dm = 1 # use PV-DM, 0 then use DBOW
+alpha = 0.025 # should be small
 
-model_path = "doc2vec_vi/models/corpus-title.model"
-corpus_path = "datasets/vi/corpus-title.tok.txt"
+model_path = "doc2vec_vi/models/corpus-title.model" # path to trained model
+corpus_path = "datasets/vi/corpus-title.tok.txt" # path to corpus file
 
 # load corpus into program
-
-
 def LoadCorpus(path):
     f = open(path, "r", encoding='utf-8')
     return f.readlines()
 
-# get n random sentences from corpus
-
-
+# get n random sentences from corpus (SHOULD BE MODIFIED)
 def PrepareBatch():
     sentences = random.sample(corpus, batch_number)
     for i, s in enumerate(sentences):
-        # TaggedDocument(tokens,tags) both params are lists
-        # Split by spaces then remove the LMU concatenation symbol '_'
+        # Split by spaces then remove the LMU (lingustically meaningful unit) concatenation symbol '_'
         sentences[i] = sentences[i].split()
         sentences[i] = [w.replace('_', ' ').lower() for w in sentences[i]]
 
+        # TaggedDocument(tokens,tags) both params are lists
         sentences[i] = TaggedDocument(sentences[i], [i])
     return sentences
 
 # train function
-
-
 def Train():
     try:
         model = Doc2Vec.load(model_path)
@@ -56,26 +51,26 @@ def Train():
         while(True):
             
             batch_start = time.time()
-            batch = PrepareBatch()
+            batch = PrepareBatch() # load batch
             
             #print('Done preparing batch {0:.2f}'.format(batch_end-batch_start) + 's')
+            model.build_vocab(batch, update=True) # build vocab for one-hot and output, MUST HAVE update=True when ADD UNSEEN documents to corpus
 
-            model.build_vocab(batch,update=True)
             for epoch in range(epochs):
                 #print("Iteration {0}".format(epoch))
-                model.train(batch,total_examples=model.corpus_count,epochs=model.epochs)
+                model.train(batch, total_examples=model.corpus_count, epochs=model.epochs) #train model
             
-            model.save(model_path)
+            model.save(model_path) # save model
             batch_end = time.time()
-            print('Finish batch {0}'.format(iter) + 'in {0:.2f}s'.format(batch_end-batch_start))
+            print('Finish batch {0}'.format(iter) + ' in {0:.2f}s'.format(batch_end-batch_start))
             iter = iter + 1
     return
 
 
 #
 load_start = time.time()
-corpus = LoadCorpus(corpus_path)
+corpus = LoadCorpus(corpus_path) # load all corpus to MEMORY, too many browser tabs can cause memory error
 load_end = time.time()
 print('Done loading corpus {0:.2f}'.format(load_end-load_start) + 's')
 
-Train()
+Train() # call train function
